f8c7da10a50f22c592f8e92d8cc91786df0f80fa
Harun Köysüren <147384148+harunkoysuren@users.noreply.github.com>
2023-11-13 10:56:42 +0300

    script.js

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ function bankaCalistir(){
 }
 
 bankaCalistir()
+
+----------------------------------------------------------------------------------------------
+
+// function hexCode(){
+//     let karakterler = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+//     let hexCodeArr = []
+
+//     for(let i = 0; i < 6 ; i++){
+//         let random = Math.floor(Math.random() * karakterler.length)
+//         hexCodeArr.push(karakterler[random])
+//     }return `#${hexCodeArr.join("")}`
+// }
+// console.log(hexCode())
